20fb604418f0acd200991be51e24d13536af375e
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2023-08-16 22:26:16 +0530

    indentations corrected

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,54 +1,54 @@
 const todoList = () => {
-    const all = [];
-  
-    const add = (todoItem) => {
-      all.push(todoItem);
-    };
-  
-    const markAsComplete = (index) => {
-      all[index].completed = true;
-    };
-  
-    const overdue = () => {
-      const today = new Date().toISOString().split("T")[0];
-      return all.filter((item) => !item.completed && item.dueDate < today);
-    };
-  
-    const dueToday = () => {
-      const today = new Date().toISOString().split("T")[0];
-      return all.filter((item) => item.dueDate === today);
-    };
-  
-    const dueLater = () => {
-      const today = new Date().toISOString().split("T")[0];
-      return all.filter((item) => !item.completed && item.dueDate > today);
-    };
-  
-    const toDisplayableList = (list) => {
-      return list
-        .map((item) => {
-          let checkbox = item.completed ? "[x]" : "[ ]";
-          const formattedDate =
-            item.dueDate !== new Date().toISOString().split("T")[0]
-              ? " " + item.dueDate
-              : "";
-          return `${checkbox} ${item.title}${formattedDate}`;
-        })
-        .join("\n");
-    };       
+  const all = [];
 
-    return {
-      all,
-      add,
-      markAsComplete,
-      overdue,
-      dueToday,
-      dueLater,
-      toDisplayableList,
-    };
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((item) => !item.completed && item.dueDate < today);
+  };
+
+  const dueToday = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((item) => item.dueDate === today);
+  };
+
+  const dueLater = () => {
+    const today = new Date().toISOString().split("T")[0];
+    return all.filter((item) => !item.completed && item.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map((item) => {
+        let checkbox = item.completed ? "[x]" : "[ ]";
+        const formattedDate =
+          item.dueDate !== new Date().toISOString().split("T")[0]
+            ? " " + item.dueDate
+            : "";
+        return `${checkbox} ${item.title}${formattedDate}`;
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
 };
-  
-module.exports= todoList;
+
+module.exports = todoList;
 
 // ####################################### #
 // DO NOT CHANGE ANYTHING BELOW THIS LINE. #
