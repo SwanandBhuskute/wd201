165d72f4686c90da0b505ab6c54127f0652f3692
Swanand Bhuskute <swanandbhuskute2003@gmail.com>
2023-09-07 10:49:57 +0530

    L5 work added

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -67,3 +67,22 @@ describe("Todolist Test Suite", () => {
     expect(dueLater().length).toBe(1);
   });
 });
+
+// const db = require("../models");
+
+// describe("Todolist Test Suite", () => {
+//   beforeAll(async () => {
+//     await db.sequelize.sync({ force: true });
+//   });
+
+//   test("Should add new todo", async () => {
+//     const todoItemsCount = await db.Todo.count();
+//     await db.Todo.addTask({
+//       title: "Test todo",
+//       completed: false,
+//       dueDate: new Date(),
+//     });
+//     const newTodoItemsCount = await db.Todo.count();
+//     expect(newTodoItemsCount).toBe(todoItemsCount + 1);
+//   });
+// });
